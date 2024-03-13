7b815962c12a9e78572dd7cc1505cc81814f21d9
George <serfertor@mail.ru>
2024-03-14 00:23:20 +0300

    Автоматизирована предобработка датасета

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ if __name__ == '__main__':
     X = dataTrain.drop('is_canceled', axis=1)
     y = dataTrain['is_canceled']
 
-    # Подготовка датасета
-    # Преобразования с помощью One-Hot encode
-    X = pd.get_dummies(X, columns=['hotel', 'meal', 'distribution_channel', 'deposit_type', 'customer_type'])
-    dataTest = pd.get_dummies(dataTest, columns=['hotel', 'meal', 'distribution_channel',
-                                                 'deposit_type', 'customer_type'])
-
-    # Преобразование с помощью Label Encoder
+    labels = X.columns
+    labels = labels.drop('reservation_status_date')
     label_encoder = LabelEncoder()
-    label_for_encode = ['arrival_date_month', 'country', 'market_segment', 'reserved_room_type', 'assigned_room_type']
-    for i in label_for_encode:
-        X[i] = label_encoder.fit_transform(X[i])
-        dataTest[i] = label_encoder.fit_transform(dataTest[i])
+
+    # Подготовка датасета
+    for i in labels:
+        if len(X[i].value_counts()) <= 4:
+            # Преобразования с помощью One-Hot encode
+            X = pd.get_dummies(X, columns=[i])
+            dataTest = pd.get_dummies(dataTest, columns=[i])
+        else:
+            # Преобразование с помощью Label Encoder
+            label_for_encode = i
+            X[i] = label_encoder.fit_transform(X[i])
+            dataTest[i] = label_encoder.fit_transform(dataTest[i])
 
     # Преобразование даты
     date = X['reservation_status_date']
@@ -42,6 +45,8 @@ if __name__ == '__main__':
     date['day'] = date['day'].astype(int)
     dataTest = pd.concat([dataTest, date], axis=1)
 
+
+    print (X.columns)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Масштабирование признаков
